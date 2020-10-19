b6a8d32caa89af08ec85fbfd92b64dcda4892a1f
Artsiom Biba <biba.artsiom@gmail.com>
2020-10-19 16:12:43 -0400

    reverse integer problem

---
+++ reverseint/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+	const reversed = n.toString().split('').reverse().join('');
+
+	// Math.sign returns '1' when passed positive num as the argument and '-1' when passed a negative num
+	return parseInt(reversed) * Math.sign(n);
+}
+
+module.exports = reverseInt;
