9012c45f4f4ce407ce0abecd1b43dd187e00e9be
Artsiom Biba <biba.artsiom@gmail.com>
2020-11-03 09:10:21 -0500

    finbonacci sequence problem

---
+++ fib/index.js
@@ -0,0 +1,61 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+// iterative solution
+
+function fib(n) {
+    const nums = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        let newNum = nums[i - 1] + nums[i -2];
+        nums.push(newNum);
+    }
+
+    return nums[n];
+}
+
+// recursive solution
+
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// memoized recursive solution
+
+// function memoize(func) {
+//     const cache = {};
+    
+//     return function(...args) {
+//         if (cache[args]) {
+//             return cache[args];
+//         }
+        
+//         const result = func.apply(this, args);
+//         cache[args] = result;
+
+//         return result;
+//     }
+// }
+
+// function slowFib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// const fib = memoize(slowFib);
+
+module.exports = fib;
