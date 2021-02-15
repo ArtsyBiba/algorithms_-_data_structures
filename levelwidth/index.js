0c9a49eaadb65a9b11235d045a879beeb36deb51
Artsy Biba <biba.artsiom@gmail.com>
2021-02-14 20:16:46 -0500

    level width problem for trees

---
+++ levelwidth/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+    const result = [0];
+    const array = [root, 's'];
+
+    while (array.length > 1) {
+        const node = array.shift();
+
+        if (node === 's') {
+            result.push(0);
+            array.push('s');
+        } else {
+            array.push(...node.children);
+            result[result.length - 1]++;
+        }
+    }
+
+    return result;
+}
+
+module.exports = levelWidth;
