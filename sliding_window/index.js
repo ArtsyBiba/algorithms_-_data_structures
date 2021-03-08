e6583db02f49bf0c671d327ff8555c7a45b75043
Artsy Biba <biba.artsiom@gmail.com>
2021-03-08 14:50:38 -0500

    vowels sliding window problem strater + setting up the tests

---
+++ sliding_window/index.js
@@ -0,0 +1,14 @@
+// Given a string s and an integer k.
+
+// Return the maximum number of vowel letters in any substring of s with length k.
+
+// Vowel letters in English are (a, e, i, o, u).
+
+// Input: s = "abciiidef", k = 3
+// Output: 3
+
+const vowels = (s, k) => {
+
+}
+
+module.exports = vowels;
---
+++ sliding_window/test.js
@@ -0,0 +1,25 @@
+const vowels = require('./index');
+
+test('Vowels is a function', () => {
+  expect(typeof vowels).toEqual('function');
+});
+
+test('returns the maximum number of vowel letters', () => {
+    expect(vowels('abciiidef', 3)).toEqual(3);
+});
+
+test('returns the maximum number of vowel letters', () => {
+    expect(vowels('aeiou', 2)).toEqual(2);
+});
+
+test('returns the maximum number of vowel letters', () => {
+    expect(vowels('leetcode', 3)).toEqual(2);
+});
+
+test('returns the maximum number of vowel letters', () => {
+    expect(vowels('rhythms', 4)).toEqual(0);
+});
+
+test('returns the maximum number of vowel letters', () => {
+    expect(vowels('tryhard', 4)).toEqual(1);
+});
