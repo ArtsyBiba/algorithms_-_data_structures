ccd703ee8ea8c3a285e94117a9f26a21e1e9713b
Artsy Biba <biba.artsiom@gmail.com>
2021-03-08 15:27:45 -0500

    adding optimized solution

--- sliding_window/index.js
+++ sliding_window/index.js
@@ -7,23 +7,47 @@
 // Input: s = "abciiidef", k = 3
 // Output: 3
 
-const vowels = (s, k) => {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    let max = 0;
+// initial solution
+// const vowels = (s, k) => {
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let max = 0;
     
-    for (let i = 0; i < s.length - k + 1; i++) {
-        const substring = s.slice(i, i + k);
-        let count = 0;
+//     for (let i = 0; i < s.length - k + 1; i++) {
+//         const substring = s.slice(i, i + k);
+//         let count = 0;
         
-        for (let letter of substring) {
-            if (vowels.includes(letter)) {
-                count++;
-            }
-        }
+//         for (let letter of substring) {
+//             if (vowels.includes(letter)) {
+//                 count++;
+//             }
+//         }
         
-        if (count > max) {
-            max = count;
+//         if (count > max) {
+//             max = count;
+//         }
+//     }
+    
+//     return max;
+// }
+
+// optimized solution
+const vowels = (s, k) => {
+    let max = 0;
+    let vowelCount = 0;
+    let vowels = new Set('aeiou');
+    
+    s.slice(0, k).split('').forEach(letter => {
+        if (vowels.has(letter)) {
+            vowelCount += 1;
         }
+    })
+    
+    max = vowelCount;
+    
+    for (let i = k; i < s.length; i++) {
+        if (vowels.has(s[i])) vowelCount += 1
+        if (vowels.has(s[i - k])) vowelCount -= 1
+        if (vowelCount > max) max = vowelCount
     }
     
     return max;
