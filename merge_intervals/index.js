f7d04d115ad3705290c957459a9ed025eb4418da
Artsy Biba <biba.artsiom@gmail.com>
2021-03-10 08:48:41 -0500

    adding first solution

--- merge_intervals/index.js
+++ merge_intervals/index.js
@@ -7,8 +7,24 @@
 // Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 
 // solution #1
-const merge = function (n) {
+const merge = function (intervals) {
+    if (!intervals.length) return intervals;
+  
+    intervals.sort((a, b) => a[0] - b[0]);
+    
+    var prev = intervals[0];
+    var res = [prev];
+    
+    for (var curr of intervals) {
+        if (curr[0] <= prev[1]) {
+          prev[1] = Math.max(prev[1], curr[1]);
+        } else {
+          res.push(curr);
+          prev = curr;
+        }
+    }
 
+    return res;
 }
 
 module.exports = merge;
