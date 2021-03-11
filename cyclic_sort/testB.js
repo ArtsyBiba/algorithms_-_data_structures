4591dc2b24669d0d6e7514d8e030009e017c10ff
Artsy Biba <biba.artsiom@gmail.com>
2021-03-11 08:52:54 -0500

    adding slow solution

--- cyclic_sort/testB.js
+++ cyclic_sort/testB.js
@@ -5,6 +5,18 @@
 // Input: arr = [2,3,4,7,11], k = 5
 // Output: 9
 
+// slow solution
 const findKthPositive = function(arr, k) {
+    const missing = [];
+    let counter = 1;
+    let max = arr[arr.length - 1] + k;
+
+    while (counter <= max) {
+        if (!arr.includes(counter)) {
+            missing.push(counter);
+        }
+        counter++;
+    }
     
+    return missing[k - 1];
 };
