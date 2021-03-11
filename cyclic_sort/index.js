0c30741ccb58f54489d6a06db677c99a9344fd0f
Artsy Biba <biba.artsiom@gmail.com>
2021-03-11 08:35:36 -0500

    solvin missing number problem

--- cyclic_sort/index.js
+++ cyclic_sort/index.js
@@ -6,7 +6,15 @@
 // extra space complexity and O(n) runtime complexity?
 
 const missingNumber = function (nums) {
-    
+  let sum = 0;
+  let total = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      total += i + 1;
+  }
+
+  return total - sum;    
 }
 
 module.exports = missingNumber;
--- cyclic_sort/test.js
+++ cyclic_sort/test.js
@@ -9,14 +9,14 @@ test('returns the only number in the range that is missing from the array', () =
 });
 
 test('returns the only number in the range that is missing from the array', () => {
-  expect(missingNumber([0,1]).toEqual(2);
+  expect(missingNumber([0,1])).toEqual(2);
 });
 
 test('returns the only number in the range that is missing from the array', () => {
-  expect(missingNumber([9,6,4,2,3,5,7,0,1]).toEqual(8);
+  expect(missingNumber([9,6,4,2,3,5,7,0,1])).toEqual(8);
 });
 
 test('returns the only number in the range that is missing from the array', () => {
-  expect(missingNumber([0]).toEqual(1);
+  expect(missingNumber([0])).toEqual(1);
 });
 
