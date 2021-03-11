840306d66b06ddb03c6769256471df3c27ab1b23
Artsy Biba <biba.artsiom@gmail.com>
2021-03-11 07:38:30 -0500

    adding cycling sort problem and setting up tests

---
+++ cyclic_sort/test.js
@@ -0,0 +1,22 @@
+const missingNumber = require('./index');
+
+test('missingNumber is a function', () => {
+  expect(typeof missingNumber).toEqual('function');
+});
+
+test('returns the only number in the range that is missing from the array', () => {
+  expect(missingNumber([3,0,1])).toEqual(2);
+});
+
+test('returns the only number in the range that is missing from the array', () => {
+  expect(missingNumber([0,1]).toEqual(2);
+});
+
+test('returns the only number in the range that is missing from the array', () => {
+  expect(missingNumber([9,6,4,2,3,5,7,0,1]).toEqual(8);
+});
+
+test('returns the only number in the range that is missing from the array', () => {
+  expect(missingNumber([0]).toEqual(1);
+});
+
