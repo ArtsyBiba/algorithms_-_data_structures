f2798d5a2e415f2f552799b442a9d919039feec1
Artsy Biba <biba.artsiom@gmail.com>
2021-03-11 08:38:02 -0500

    setting up another missing number problem

---
+++ cyclic_sort/testB.js
@@ -0,0 +1,10 @@
+// Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+// Find the kth positive integer that is missing from this array.
+
+// Input: arr = [2,3,4,7,11], k = 5
+// Output: 9
+
+const findKthPositive = function(arr, k) {
+    
+};
