6cfdf07adb677c8ddf0be71fec696eb355c2f084
Artsy Biba <biba.artsiom@gmail.com>
2021-03-13 09:19:34 -0500

    adding first solution to tree depth first search problem

--- tree_dfs/index.js
+++ tree_dfs/index.js
@@ -3,8 +3,25 @@
 // A binary tree's maximum depth is the number of nodes along 
 // the longest path from the root node down to the farthest leaf node.
 
+// solution #1
 const maxDepth = function (root) {
-
+    if (!root) return 0;
+    
+    const arr = [root];
+    let levels = 0;
+    
+    while (arr.length) {
+        let size = arr.length;
+        
+        for (let i = 0; i < size; i++) {
+            const node = arr.shift();
+            if (node.right) arr.push(node.right);
+            if (node.left) arr.push(node.left);
+        }
+        levels++;
+    }
+    
+    return levels;
 }
 
 module.exports = maxDepth;
