5e65a3e39941dc2725db047c06b1024c1fb3b651
Artsy Biba <biba.artsiom@gmail.com>
2021-03-13 08:27:59 -0500

    solving zigzag zigzag level order traversal

--- tree_bfs/indexB.js
+++ tree_bfs/indexB.js
@@ -4,7 +4,32 @@
 // then right to left for the next level and alternate between).
 
 const zigzagLevelOrder = function (root) {
-
+    if (!root) return [];
+    
+    const arr = [root];
+    const result = [];
+    let counter = 1;
+    
+    while (arr.length) {
+        const temp = [];
+        const size = arr.length;
+        
+        for (let i = 0; i < size; i++) {
+            const node = arr.shift();
+            temp.push(node.val);
+            if (node.left) arr.push(node.left);
+            if (node.right) arr.push(node.right);
+        }
+        
+        if (counter % 2 === 0) {
+            result.push(temp.reverse());
+        } else {
+            result.push(temp);
+        }
+        counter++;
+    }
+    
+    return result;
 }
 
 module.exports = zigzagLevelOrder;
