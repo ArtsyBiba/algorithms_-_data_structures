08a71ca15f1760d91be0b60ce24fad3efc4fa98d
Artsy Biba <biba.artsiom@gmail.com>
2021-03-12 19:16:02 -0500

    adding binary tree level oreder traversal

--- tree_bfs/index.js
+++ tree_bfs/index.js
@@ -3,7 +3,30 @@
 // (i.e., from left to right, level by level).
 
 const levelOrder = function (root) {
-
+  if (!root) return [];
+    
+  const stack = [root];
+  const result = [];
+  
+  while (stack.length > 0) {
+      const temp = [];
+      const size = stack.length
+      
+      for (let i = 0; i < size; i++) {
+          const node = stack.shift();
+          temp.push(node.val);
+          if (node.left) {
+              stack.push(node.left);
+          }
+          if (node.right) {
+              stack.push(node.right);
+          }
+      }
+      
+      result.push(temp);
+  }
+  
+  return result;
 }
 
 module.exports = levelOrder;
