ac40a404b58ae98be581e969ff08d18f338eddc7
Artsy Biba <biba.artsiom@gmail.com>
2021-03-16 09:00:49 -0400

    adding binary search solution

--- binary_search/search_insert_position.js
+++ binary_search/search_insert_position.js
@@ -2,6 +2,22 @@
 // return the index if the target is found. If not, return the 
 // index where it would be if it were inserted in order.
 
+// binary search solution
 var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
     
+    while (start <= end) {
+        const mid = Math.floor((start + end) / 2);
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    
+    return start
 };
