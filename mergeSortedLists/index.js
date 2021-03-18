ea7b69be0e51a120041ccf08fd864c24ac2ad29e
Artsy Biba <biba.artsiom@gmail.com>
2021-03-18 17:42:38 -0400

    adding brute force solution

--- mergeSortedLists/index.js
+++ mergeSortedLists/index.js
@@ -3,8 +3,31 @@
 
 // Merge all the linked-lists into one sorted linked-list and return it.
 
+// brute force solution
 const mergeKLists = function steps(lists) {
-
+    if (!lists) return [];
+    if (lists === []) return [[]];
+    const arr = [];
+    
+    for (let list of lists) {
+        let head = list;
+        
+        while (head) {
+            arr.push(head.val);
+            head = head.next;
+        }
+    }
+    
+    arr.sort((a, b) => a - b);
+    let head = new ListNode(arr[0]);
+    let selectedNode = head;
+    
+    for (let i = 1; i < arr.length; i++) {
+        selectedNode.next = new ListNode(arr[i]);
+        selectedNode = selectedNode.next;
+    }
+    
+    return head;
 }
 
 module.exports = mergeKLists;
