ccb329a6ea64b66972aa69374e604abfbf76b975
Artsy Biba <biba.artsiom@gmail.com>
2021-03-19 15:06:07 -0400

    adding initial solution

--- matrixes/diagonalSum.js
+++ matrixes/diagonalSum.js
@@ -3,6 +3,22 @@
 // Only include the sum of all the elements on the primary diagonal 
 // and all the elements on the secondary diagonal that are not part of the primary diagonal.
 
+// solution #1
 var diagonalSum = function(mat) {
+    let sum = 0;
+    let left = 0;
+    let right = mat[0].length - 1;
+
+    for (let i = 0; i < mat.length; i++) {
+        sum += mat[i][left];
+        mat[i][left] = 0;
+        left++;
+        
+        sum += mat[i][right];
+        mat[i][right] = 0;
+        right--;
+    }
     
+    return sum;
 };
+
