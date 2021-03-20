f6504c7ab00905aaea1573976cc1a80804b733fe
Artsy Biba <biba.artsiom@gmail.com>
2021-03-20 11:06:02 -0400

    adding binary search solution

--- matrixes/kSmallestElem.js
+++ matrixes/kSmallestElem.js
@@ -20,3 +20,28 @@ var kthSmallest = function(matrix, k) {
     
     return arr[k - 1]
 };
+
+// binary search solution
+var kthSmallest = function(matrix, k) {
+    let lo = matrix[0][0]
+    let hi = matrix[matrix.length - 1][matrix[0].length - 1]; 
+    
+    while (lo < hi) {
+        let mid = lo + Math.floor((hi - lo) / 2);
+        let count = 0;
+        
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] <= mid) {
+                    count++;
+                } else break;
+            }
+        }
+        
+        if (count < k) {
+            lo = mid + 1;
+        } else hi = mid;
+    }
+    
+    return lo
+}
