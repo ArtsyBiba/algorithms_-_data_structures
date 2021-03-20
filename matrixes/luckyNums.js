06d5ebf69e3d503c2098494226e8809f18c5ed0e
Artsy Biba <biba.artsiom@gmail.com>
2021-03-20 09:54:22 -0400

    adding brute force solution

--- matrixes/luckyNums.js
+++ matrixes/luckyNums.js
@@ -4,6 +4,35 @@
 // A lucky number is an element of the matrix such 
 // that it is the minimum element in its row and maximum in its column.
 
+// brute force solution
 var luckyNumbers  = function(matrix) {
+    const mins = [];
+    const maxs = [];
     
+    for (let i = 0; i < matrix.length; i++) {
+        let min = matrix[i][0];
+        
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] < min) min = matrix[i][j];
+            let max = matrix[0][j];
+                
+            for (let k = 0; k < matrix.length; k++) {
+                if (matrix[k][j] > max) max = matrix[k][j];
+            }
+            
+            maxs.push(max);
+        }
+        
+        mins.push(min);
+    }
+
+    if (matrix.length === 1) return mins;
+    
+    const result = [];
+    
+    for (let num of mins) {
+        if (maxs.includes(num)) result.push(num);
+    }
+    
+    return result;
 };
