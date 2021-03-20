c5a97ffd2f6809a1c3343566f1c60051f6bbff55
Artsy Biba <biba.artsiom@gmail.com>
2021-03-20 10:07:25 -0400

    adding improved solution

--- matrixes/luckyNums.js
+++ matrixes/luckyNums.js
@@ -36,3 +36,39 @@ var luckyNumbers  = function(matrix) {
     
     return result;
 };
+
+// improved solution
+var luckyNumbers  = function(matrix) {
+    const mins = [];
+    const maxs = [];
+    
+    for (let i = 0; i < matrix.length; i++) {
+        let min = matrix[i][0];
+        
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] < min) min = matrix[i][j];
+        }
+        
+        mins.push(min);
+    }
+    
+    for (let i = 0; i < matrix[0].length; i++) {
+        let max = matrix[0][i];
+        
+        for (let j = 0; j < matrix.length; j++) {
+            if (matrix[j][i] > max) max = matrix[j][i];
+        }
+        
+        maxs.push(max);
+    }
+
+    if (matrix.length === 1) return mins;
+    
+    const result = [];
+    
+    for (let num of mins) {
+        if (maxs.includes(num)) result.push(num);
+    }
+    
+    return result;
+}
