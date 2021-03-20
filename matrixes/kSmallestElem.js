7fcb9eaa0ecf0ceabecb1cfeb2cef97ba2c2bd80
Artsy Biba <biba.artsiom@gmail.com>
2021-03-20 10:38:43 -0400

    setting up k smallest element problem

---
+++ matrixes/kSmallestElem.js
@@ -0,0 +1,11 @@
+// Given an n x n matrix where each of 
+// the rows and columns are sorted in 
+// ascending order, return the kth smallest 
+// element in the matrix.
+
+// Note that it is the kth smallest element 
+// in the sorted order, not the kth distinct element.
+
+var kthSmallest = function(matrix, k) {
+    
+};
