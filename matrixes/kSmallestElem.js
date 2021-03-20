cd79120ec1c882f010ea126395156314eacdc72b
Artsy Biba <biba.artsiom@gmail.com>
2021-03-20 10:58:01 -0400

    adding brute force solution

--- matrixes/kSmallestElem.js
+++ matrixes/kSmallestElem.js
@@ -6,6 +6,17 @@
 // Note that it is the kth smallest element 
 // in the sorted order, not the kth distinct element.
 
+// brute force solution
 var kthSmallest = function(matrix, k) {
+    const arr = [];
     
+    for (let row of matrix) {
+        for (let num of row) {
+            arr.push(num);
+        }
+    }
+    
+    arr.sort((a, b) => a - b);
+    
+    return arr[k - 1]
 };
