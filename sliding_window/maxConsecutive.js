d965143f6bafa686d624d5f28d2e3f20d7a48d8b
Artsy Biba <biba.artsiom@gmail.com>
2021-03-21 11:13:23 -0400

    sliding window solution

--- sliding_window/maxConsecutive.js
+++ sliding_window/maxConsecutive.js
@@ -3,5 +3,20 @@
 // Return the length of the longest (contiguous) subarray that contains only 1s. 
 
 var longestOnes = function(A, K) {
+    let longest = 0;
+    let zeroCount = 0;
+    let start = 0;
     
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] === 0) zeroCount++;
+
+        if (zeroCount > K) {
+            if (A[start] === 0) zeroCount--;
+            start++;
+        }
+        
+        longest = Math.max(longest, i - start + 1);
+    }
+    
+    return longest;
 };
