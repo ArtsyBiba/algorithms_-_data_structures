84312b42728b717dbf8807e7700c0f473ff1d8ed
Artsy Biba <biba.artsiom@gmail.com>
2021-03-22 14:59:11 -0400

    adding brute force solution

--- sorting/squaresOfSorted.js
+++ sorting/squaresOfSorted.js
@@ -1,6 +1,9 @@
 // Given an integer array nums sorted in non-decreasing order, 
 // return an array of the squares of each number sorted in non-decreasing order.
 
+// brute force solution
 var sortedSquares = function(nums) {
+    const squared = nums.map(num => num * num);
     
+    return squared.sort((a, b) => a - b);
 };
