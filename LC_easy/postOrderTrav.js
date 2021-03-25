e3329e0fe687eb10298c16b7c2f535b1ae6f7274
Artsy Biba <biba.artsiom@gmail.com>
2021-03-25 09:18:59 -0400

    setting up post order tree traversal problem

---
+++ LC_easy/postOrderTrav.js
@@ -0,0 +1,10 @@
+// Given the root of an n-ary tree, return 
+// the postorder traversal of its nodes' values.
+
+// Nary-Tree input serialization is represented 
+// in their level order traversal. Each group 
+// of children is separated by the null value (See examples)
+
+var postorder = function(root) {
+    
+};
