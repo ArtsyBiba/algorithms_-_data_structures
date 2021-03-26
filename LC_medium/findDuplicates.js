8f68f65faf9973e5ab9375883e62d3cd364c2312
Artsy Biba <biba.artsiom@gmail.com>
2021-03-26 15:21:55 -0400

    coming up with initial solution

--- LC_medium/findDuplicates.js
+++ LC_medium/findDuplicates.js
@@ -6,5 +6,20 @@
 // Could you do it without extra space and in O(n) runtime?
 
 var findDuplicates = function(nums) {
+    const map = {};
     
+    for (let num of nums) {
+        if (map[num]) {
+            map[num] = map[num] + 1;
+        } else map[num] = 1;
+    }
+    
+    const result = [];
+    for (let key in map) {
+        if (map[key] > 1) {
+            result.push(key);
+        }
+    }
+    
+    return result;
 };
