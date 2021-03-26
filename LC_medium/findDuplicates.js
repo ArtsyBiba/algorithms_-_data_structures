66c7fabc522c92998acceb73554cf248f51bb9b6
Artsy Biba <biba.artsiom@gmail.com>
2021-03-26 15:04:13 -0400

    setting up find duplicates problem

---
+++ LC_medium/findDuplicates.js
@@ -0,0 +1,10 @@
+// Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), 
+// some elements appear twice and others appear once.
+
+// Find all the elements that appear twice in this array.
+
+// Could you do it without extra space and in O(n) runtime?
+
+var findDuplicates = function(nums) {
+    
+};
