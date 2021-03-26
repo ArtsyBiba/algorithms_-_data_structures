15ce7a1a3600e3a36d99274b9f193d52f9dd9641
Artsy Biba <biba.artsiom@gmail.com>
2021-03-26 15:01:11 -0400

    solving max difference between node and anc problem

--- LC_medium/maxDiffBtwNode&Anc.js
+++ LC_medium/maxDiffBtwNode&Anc.js
@@ -6,5 +6,23 @@
 // A is equal to B, or any child of A is an ancestor of B.
 
 var maxAncestorDiff = function(root) {
+    if (!root) return 0;
     
+    let result = 0;
+    helper(root, root.val, root.val);
+    
+    function helper(root, max, min) {
+        if (!root) {
+            result = Math.max(result, max - min);
+            return;
+        }
+        
+        max = Math.max(max, root.val);
+        min = Math.min(min, root.val);
+        
+        helper(root.left, max, min);
+        helper(root.right, max, min);
+    }
+
+    return result;
 };
