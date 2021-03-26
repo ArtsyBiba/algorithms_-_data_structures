ffbd32907776633e180662b97b3087bd98dbed47
Artsy Biba <biba.artsiom@gmail.com>
2021-03-26 14:42:45 -0400

    adding alternative solution

--- LC_easy/reverseString.js
+++ LC_easy/reverseString.js
@@ -1,5 +1,22 @@
 // Write a function that reverses a string. The input string is given as an array of characters s.
 
+// initial solution
 var reverseString = function(s) {
     return s.reverse()
+};
+
+// alternative solution
+var reverseString = function(s) {
+    let start = 0;
+    let end = s.length - 1;
+    
+    while (start < end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+    
+    return s;
 };
