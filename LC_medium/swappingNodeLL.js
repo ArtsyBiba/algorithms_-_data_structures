a0f782e627db906a428110e1a84e4652b4fa6681
Artsy Biba <biba.artsiom@gmail.com>
2021-03-27 11:23:35 -0400

    adding brute force solution

--- LC_medium/swappingNodeLL.js
+++ LC_medium/swappingNodeLL.js
@@ -3,6 +3,26 @@
 // Return the head of the linked list after swapping the values 
 // of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
 
+// brute force solution
 var swapNodes = function(head, k) {
+    const arr = [];
+    let curr = head;
     
+    while (curr) {
+        arr.push(curr.val);
+        curr = curr.next;
+    }
+    
+    let temp = arr[k - 1];
+    arr[k - 1] = arr[arr.length - k];
+    arr[arr.length - k] = temp;
+    
+    let newCurr = head;
+    while (newCurr) {
+        let val = arr.shift();
+        newCurr.val = val;
+        newCurr = newCurr.next;
+    }
+    
+    return head;
 };
