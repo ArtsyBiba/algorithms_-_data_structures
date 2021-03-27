d2657323a6742ffedd3e379ea0bb3c36ee086d2a
Artsy Biba <biba.artsiom@gmail.com>
2021-03-27 11:54:11 -0400

    adding recursive solution

--- LC_medium/nArrOrderTraversal.js
+++ LC_medium/nArrOrderTraversal.js
@@ -3,6 +3,26 @@
 // Nary-Tree input serialization is represented in their level order traversal, 
 // each group of children is separated by the null value (See examples).
 
+// recursive solution
 var levelOrder = function(root) {
+    if (!root) return [];
+    const arr = [root];
+    const result = [];
     
+    while (arr.length) {
+        const size = arr.length;
+        const level = [];
+        
+        for (let i = 0; i < size; i++) {
+            const node = arr.shift();
+            
+            for (const child of node.children) {
+                arr.push(child);
+            }
+            level.push(node.val);
+        }
+        result.push(level);
+    }
+    
+    return result;
 };
