cc6f9439728858e9948e6b04d18b0d8533394d68
Artsy Biba <biba.artsiom@gmail.com>
2021-03-28 16:35:34 -0400

    solving the problem using brute force solution

--- LC_medium/reduceArrSize.js
+++ LC_medium/reduceArrSize.js
@@ -4,6 +4,32 @@
 // Return the minimum size of the set so that at least half 
 // of the integers of the array are removed.
 
+// brute force solution
 var minSetSize = function(arr) {
+    let map = {};
+    let min = 0;
+    let sum = 0;
+    let index = 0;
     
+    for (let num of arr) {
+        if (map[num]) {
+            map[num]++;
+        } else map[num] = 1;
+    }
+    
+    const pairs = [];
+    
+    for (let key in map) {
+        pairs.push([Number(key), map[key]]);
+    }
+    
+    pairs.sort((a,b) => b[1] - a[1]);
+    
+    while (sum < Math.floor(arr.length / 2)) {
+        sum += pairs[index][1];
+        index++;
+        min++;
+    }
+    
+    return min;
 };
