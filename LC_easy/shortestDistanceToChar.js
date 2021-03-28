fc0e3dff1e5b6824229703ba4b500bde4531d654
Artsy Biba <biba.artsiom@gmail.com>
2021-03-28 15:45:32 -0400

    adding solution with helper function

--- LC_easy/shortestDistanceToChar.js
+++ LC_easy/shortestDistanceToChar.js
@@ -7,5 +7,27 @@
 // where abs is the absolute value function.
 
 var shortestToChar = function(s, c) {
+    var shortestToChar = function(s, c) {
+        let arr = [];
+        
+        for (let i = 0; i < s.length; i++) {
+            let closest = findClosest(i, s, c); 
+            
+            arr.push(closest);
+        }
+        
+        return arr
+    };
     
+    var findClosest = function(i, s, c) {
+        if (s[i] === c) return 0
+        
+        let j = 1;
+        while (i >= 0 || i <= s.length) {
+            if (s[i + j] === c || s[i - j] === c) {
+                return j;
+            }
+            j++;
+        }
+    }
 };
