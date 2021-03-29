71857e48ddcd4e304a1c8f496b558e7e57c8cd23
Artsy Biba <biba.artsiom@gmail.com>
2021-03-29 09:12:30 -0400

    solving bt inorder traversal problem

--- LC_medium/BTinorderTrav.js
+++ LC_medium/BTinorderTrav.js
@@ -1,5 +1,19 @@
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
 var inorderTraversal = function(root) {
+    const stack = [];
+    const result = [];
     
+    while (root || stack.length) {
+        if (root) {
+            stack.push(root);
+            root = root.left;
+        } else {
+            root = stack.pop();
+            result.push(root.val);
+            root = root.right;
+        }
+    }
+    
+    return result;
 };
