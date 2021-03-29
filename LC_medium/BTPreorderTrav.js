650da11835c0e4ffb0c4bf46ff0481b280c7fb68
Artsy Biba <biba.artsiom@gmail.com>
2021-03-29 09:28:03 -0400

    solving bt preorder traversal problem

--- LC_medium/BTPreorderTrav.js
+++ LC_medium/BTPreorderTrav.js
@@ -1,5 +1,16 @@
 // Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
 var preorderTraversal = function(root) {
+    if (!root) return [];
+    const stack = [root];
+    const result = [];
     
+    while (stack.length) {
+        const node = stack.pop();
+        result.push(node.val);
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+    
+    return result;
 };
