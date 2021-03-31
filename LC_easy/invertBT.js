99dc76352e90ad3c15935f843d22287e33bcf050
Artsy Biba <biba.artsiom@gmail.com>
2021-03-31 10:14:17 -0400

    adding initial solution

--- LC_easy/invertBT.js
+++ LC_easy/invertBT.js
@@ -1,5 +1,15 @@
 // Given the root of a binary tree, invert the tree, and return its root.
 
+// initial solution
 var invertTree = function(root) {
+    var stack = [root];
     
+    while (stack.length) {
+        var node = stack.pop();
+        if (!node) continue;
+        [node.left, node.right] = [node.right, node.left];
+        stack.push(node.left, node.right);
+    }
+    
+    return root;
 };
