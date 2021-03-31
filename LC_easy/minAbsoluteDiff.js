f4ff40fb49dc0d87f485ba2aa842924bced94832
Artsy Biba <biba.artsiom@gmail.com>
2021-03-31 10:58:22 -0400

    solving the min absolute difference problem

--- LC_easy/minAbsoluteDiff.js
+++ LC_easy/minAbsoluteDiff.js
@@ -8,5 +8,21 @@
 // b - a equals to the minimum absolute difference of any two elements in arr
 
 var minimumAbsDifference = function(arr) {
+    let result = [];
+    let minNum = Number.MAX_SAFE_INTEGER;
     
+	arr.sort((a, b) => a - b);
+    
+	for (let i = 0; i < arr.length - 1; i++) {
+		let diff = arr[i + 1] - arr[i];
+        
+		if (diff < minNum) {
+			result = [[arr[i], arr[i + 1]]];
+			minNum = diff;
+		} else if (diff === minNum) {
+			result.push([arr[i], arr[i + 1]]);
+		}
+	}
+    
+	return result
 };
