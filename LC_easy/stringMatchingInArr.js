e13b9295717ff30a11be34b08bcf1867ad39a07d
Artsy Biba <biba.artsiom@gmail.com>
2021-04-01 14:47:36 -0400

    solving string mathcing in array problem

--- LC_easy/stringMatchingInArr.js
+++ LC_easy/stringMatchingInArr.js
@@ -5,5 +5,13 @@
 // if can be obtained removing some characters to left and/or right side of words[j].
 
 var stringMatching = function(words) {
+    const result = [];
     
+    for (let word of words) {
+        if (words.filter(w => w.includes(word)).length > 1) {
+            result.push(word);
+        }
+    }
+    
+    return result;
 };
