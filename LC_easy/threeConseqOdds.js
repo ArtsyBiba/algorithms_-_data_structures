f83e1d390960c83375586dd4e5fe1dcfa86db73b
Artsy Biba <biba.artsiom@gmail.com>
2021-04-01 14:16:59 -0400

    solving three consequtive odds problem

--- LC_easy/threeConseqOdds.js
+++ LC_easy/threeConseqOdds.js
@@ -2,5 +2,16 @@
 // three consecutive odd numbers in the array. Otherwise, return false.
 
 var threeConsecutiveOdds = function(arr) {
+    let counter = 0;
     
+    for (let num of arr) {
+        if (num % 2 !== 0) {
+            counter++;
+            if (counter === 3) {
+                return true;
+            }
+        } else counter = 0;
+    }
+    
+    return false;
 };
