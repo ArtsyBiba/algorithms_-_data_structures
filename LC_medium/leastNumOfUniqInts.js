797982fff7b011477f3e990d52328535ad89ed7d
Artsy Biba <biba.artsiom@gmail.com>
2021-04-02 11:44:39 -0400

    solving least nums of uniq ints after k removals

--- LC_medium/leastNumOfUniqInts.js
+++ LC_medium/leastNumOfUniqInts.js
@@ -1,6 +1,28 @@
 // Given an array of integers arr and an integer k. 
 // Find the least number of unique integers after removing exactly k elements.
 
+// brute force solution
 var findLeastNumOfUniqueInts = function(arr, k) {
+    const map = {};
     
+    for (let num of arr) {
+        if (map[num]) {
+            map[num]++;
+        } else map[num] = 1;
+    }
+    
+    const freq = [];
+    for (let key in map) {
+        freq.push(map[key]);
+    }
+    
+    freq.sort((a,b) => a - b);
+    console.log(freq)
+    
+    while (k >= freq[0]) {
+        const val = freq.shift();
+        k -= val;
+    }
+    
+    return freq.length
 };
