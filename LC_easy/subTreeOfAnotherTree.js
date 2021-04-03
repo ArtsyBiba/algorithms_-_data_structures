10d3e4054cd960db149d405e90b7b489b643b3e0
Artsy Biba <biba.artsiom@gmail.com>
2021-04-03 11:59:06 -0400

    solving substree of another tree problem

--- LC_easy/subTreeOfAnotherTree.js
+++ LC_easy/subTreeOfAnotherTree.js
@@ -6,5 +6,21 @@
 // The tree s could also be considered as a subtree of itself.
 
 var isSubtree = function(s, t) {
+    const isSameTree = (t1, t2) => {
+        if (!t1 && !t2) return true;
+        if (!t1 || !t2) return false;
+        if (t1.val !== t2.val) return false;
+        
+        return isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
+    }
     
+    const helper = (t1, t2) => {
+        if (!t1) return false;
+ 
+        if (isSameTree(t1, t2)) return true;
+        
+        return helper(t1.left, t2) || helper(t1.right, t2);
+    }
+    
+    return helper(s, t)
 };
