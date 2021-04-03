4f1fef358049edd31a2949e6f866ddc66f04dc5f
Artsy Biba <biba.artsiom@gmail.com>
2021-04-03 11:36:09 -0400

    solving find pivot index problem

--- LC_easy/findPivotIndex.js
+++ LC_easy/findPivotIndex.js
@@ -10,5 +10,14 @@
 // Return the leftmost pivot index. If no such index exists, return -1.
 
 var pivotIndex = function(nums) {
+    let leftSum = 0;
+    let rightSum = nums.reduce((a,b) => a + b);
     
+    for (let i = 0; i < nums.length; i++) {
+        rightSum -= nums[i];
+        if (leftSum === rightSum) return i;
+        leftSum += nums[i];
+    }
+	
+    return -1;
 };
