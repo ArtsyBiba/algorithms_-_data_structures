9929ef80ef192cb62ce0150c188eacc5378ecd47
Artsy Biba <biba.artsiom@gmail.com>
2021-04-04 19:23:05 -0400

    solving k closest points to origin problem

--- LC_medium/closestPointsToOrigin.js
+++ LC_medium/closestPointsToOrigin.js
@@ -9,5 +9,20 @@
 // is guaranteed to be unique (except for the order that it is in).
 
 var kClosest = function(points, k) {
+    const arr = [];
     
+    for (let point of points) {
+        arr.push([point[0], point[1], Math.sqrt((point[0] ** 2) + (point[1] ** 2))]);
+    }
+    
+    arr.sort((a, b) => a[2] - b[2]);
+    
+    const result = [];
+    while (k > 0) {
+        let temp = arr.shift();
+        result.push([temp[0], temp[1]]);
+        k--;
+    }
+    
+    return result; 
 };
