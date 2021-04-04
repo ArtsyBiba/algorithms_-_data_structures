effc03b04f3c7e4e2a58d6342a709986cd4bbaed
Artsy Biba <biba.artsiom@gmail.com>
2021-04-04 19:00:00 -0400

    solving uncommon words from two sentences problem

--- LC_easy/uncommonWords.js
+++ LC_easy/uncommonWords.js
@@ -9,5 +9,26 @@
 // You may return the list in any order.
 
 var uncommonFromSentences = function(A, B) {
+    const map = {};
+    const splitA = A.split(' ');
+    const splitB = B.split(' ');
     
+    for (let word of splitA) {
+        if (map[word]) {
+            map[word]++;
+        } else map[word] = 1;
+    }
+    
+    for (let word of splitB) {
+        if (map[word]) {
+            map[word]++;
+        } else map[word] = 1;
+    }
+    
+    const result = [];
+    for (let key in map) {
+        if (map[key] === 1) result.push(key);
+    }
+    
+    return result;
 };
