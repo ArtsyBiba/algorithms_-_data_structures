4422c185f8d9d33d0024b0b866300e075d6f46fd
Artsy Biba <biba.artsiom@gmail.com>
2021-04-05 11:00:21 -0400

    solving subarray sums divisible by K problem

--- LC_medium/subarrSumsDivByK.js
+++ LC_medium/subarrSumsDivByK.js
@@ -2,5 +2,21 @@
 // (contiguous, non-empty) subarrays that have a sum divisible by K.
 
 var subarraysDivByK = function(A, K) {
+    let map = {}; 
+    let sum = 0;
     
+    for (let i = 0; i < A.length; i++) { 
+      sum += A[i];
+      const key = ((sum % K) + K) % K;
+      map[key] = map[key] + 1 || 1; 
+    } 
+    
+    let result = 0;
+    for (let i = 0; i < K; i++) {
+        if (map[i] > 1) {
+            result += (map[i] * (map[i] - 1)) / 2;
+		}
+	}
+    
+    return result + (map[0] || 0);
 };
