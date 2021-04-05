7711a07ae4c77fd546ad9211583659080ea9cc7d
Artsy Biba <biba.artsiom@gmail.com>
2021-04-05 10:13:46 -0400

    solving k largest element in array problem

--- LC_medium/kLargestElInArr.js
+++ LC_medium/kLargestElInArr.js
@@ -3,5 +3,7 @@
 // Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 var findKthLargest = function(nums, k) {
+    const sorted = nums.sort((a, b) => b - a);
     
+    return sorted[k - 1];
 };
