4a5fc5ac91fc92a44a15335b01180bfc424dbad0
Artsy Biba <biba.artsiom@gmail.com>
2021-04-06 09:41:46 -0400

    brute force solution for subarray sum equals K problem

--- LC_medium/subarrSumEqualsK.js
+++ LC_medium/subarrSumEqualsK.js
@@ -3,5 +3,18 @@
 // whose sum equals to k.
 
 var subarraySum = function(nums, k) {
+    let count = 0;
     
+    for (let i = 0; i < nums.length; i++) {
+        let sum = 0;
+        for (let j = i; j < nums.length; j++) {
+            sum += nums[j];
+            console.log(sum)
+            if (sum === k) {
+                count++;
+            }
+        }
+    }
+    
+    return count;
 };
