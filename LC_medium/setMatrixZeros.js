4ff3f49224417d8c42d8a26fee5ba389fa59315f
Artsy Biba <biba.artsiom@gmail.com>
2021-04-06 10:44:52 -0400

    solving set matrix zeroes problem

--- LC_medium/setMatrixZeros.js
+++ LC_medium/setMatrixZeros.js
@@ -7,5 +7,51 @@
 // Could you devise a constant space solution?
 
 var setZeroes = function(matrix) {
+    var setZeroes = function(matrix) {
+        let firstColHasZero = false;
+        let firstRowHasZero = false;
     
+        for (let i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] === 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+    
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] === 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+    
+        for (let i = 1; i < matrix.length; i++) {
+            for (let j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] === 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+    
+        for (let i = 1; i < matrix.length; i++) {
+            for (let j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    
+        if (firstColHasZero) {
+            for (let i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    
+        if (firstRowHasZero) {
+            for (let j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    };
 };
