908607b118d0a6512e5eeaece252c6a9b7cce0ef
Artsy Biba <biba.artsiom@gmail.com>
2021-04-07 19:06:52 -0400

    solving jump game problem

--- LC_medium/jumpGame.js
+++ LC_medium/jumpGame.js
@@ -7,5 +7,17 @@
 // You can assume that you can always reach the last index.
 
 var jump = function(nums) {
+    let newMax = 0;
+    let jump = 0;
+    let oldMax = 0;
     
+    for (let i = 0; i < nums.length - 1; i++) {
+        newMax = Math.max(newMax, i + nums[i]);
+        if (i === oldMax) {
+            jump++;
+            oldMax = newMax;
+        }
+    }
+    
+    return jump;
 };
