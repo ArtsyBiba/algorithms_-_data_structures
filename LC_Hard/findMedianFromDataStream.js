d7cadec821b3e8e77dde053132add30d11443fa3
Artsy Biba <biba.artsiom@gmail.com>
2021-04-07 10:52:00 -0400

    solving find median from data stream problem

--- LC_Hard/findMedianFromDataStream.js
+++ LC_Hard/findMedianFromDataStream.js
@@ -9,5 +9,32 @@
 // double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
 
 var MedianFinder = function() {
+    this.arr = [];
+};
+
+MedianFinder.prototype.addNum = function(num) { 
+    let low = 0 ; 
+    let high = this.arr.length - 1;
     
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2);
+            
+        if (this.arr[mid] < num) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    this.arr.splice(low, 0, num);
+};
+
+MedianFinder.prototype.findMedian = function() {
+    if (this.arr.length % 2 === 0) {
+        const mid = this.arr.length / 2;
+        return (this.arr[mid] + this.arr[mid - 1]) / 2;
+    } else {
+        const mid = Math.floor(this.arr.length / 2);
+        return (this.arr[mid]);
+    }
 };
