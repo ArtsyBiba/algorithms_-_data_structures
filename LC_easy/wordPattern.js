84eac836219d91f3538f1dab4a53c0a509e7e9da
Artsy Biba <biba.artsiom@gmail.com>
2021-04-08 14:19:07 -0400

    brute force solution for word pattern problem

--- LC_easy/wordPattern.js
+++ LC_easy/wordPattern.js
@@ -3,6 +3,19 @@
 // Here follow means a full match, such that there is a bijection between
 // a letter in pattern and a non-empty word in s.
 
+// brute force solution
 var wordPattern = function(pattern, s) {
+    const map = {};
+    const split = s.split(" ");
+    if (split.length !== pattern.length) return false;
     
+    for (let i = 0; i < pattern.length; i++) {
+        if (map[pattern[i]] && map[pattern[i]] !== split[i]) return false;
+        if (!map[pattern[i]]) {
+            if (Object.values(map).includes(split[i])) return false;
+            map[pattern[i]] = split[i];
+        }
+    }
+    
+    return true;
 };
