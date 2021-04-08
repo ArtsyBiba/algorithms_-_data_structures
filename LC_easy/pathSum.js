c7bedec1eb2233b4845238f5f655be56fae08fa3
Artsy Biba <biba.artsiom@gmail.com>
2021-04-08 14:00:27 -0400

    iterative solution for path sum problem

--- LC_easy/pathSum.js
+++ LC_easy/pathSum.js
@@ -14,3 +14,27 @@ var hasPathSum = function(root, targetSum) {
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 };
+
+// iterative solution
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+    
+    const queue = [root];
+    
+    while (queue.length > 0) {
+        let cur = queue.shift();
+        
+        if (!cur.left && !cur.right && cur.val == targetSum) return true;
+
+        if (cur.left) {
+            cur.left.val += cur.val;
+            queue.push(cur.left);
+        }
+        if (cur.right) {
+            cur.right.val += cur.val;
+            queue.push(cur.right);
+        }
+    }
+    
+    return false;
+};
