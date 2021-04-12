29c12483a9a3301b2c886661fccbecea2061f9da
Artsy Biba <biba.artsiom@gmail.com>
2021-04-12 08:58:32 -0400

    setting up intersection of two linked lists problem

---
+++ LC_easy/intersectinOfLinkedLists.js
@@ -0,0 +1,11 @@
+// Given the heads of two singly linked-lists headA and headB, 
+// return the node at which the two lists intersect. If the two 
+// linked lists have no intersection at all, return null.
+
+// It is guaranteed that there are no cycles anywhere in the entire linked structure.
+
+// Note that the linked lists must retain their original structure after the function returns.
+
+var getIntersectionNode = function(headA, headB) {
+    
+};
