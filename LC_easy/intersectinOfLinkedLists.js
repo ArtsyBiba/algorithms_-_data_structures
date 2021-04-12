793facd29b4f6fc08579debf22853c672ad2260e
Artsy Biba <biba.artsiom@gmail.com>
2021-04-12 09:55:17 -0400

    solving intersection of two linked lists problem

--- LC_easy/intersectinOfLinkedLists.js
+++ LC_easy/intersectinOfLinkedLists.js
@@ -7,5 +7,27 @@
 // Note that the linked lists must retain their original structure after the function returns.
 
 var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) return null;
+    let curA = headA;
+    let curB = headB;
     
+    while (curA !== curB) {
+        if (curA.next) {
+            curA = curA.next;
+        } else {
+            if (!curB.next) {
+                curA = null;
+                break;
+            }
+            curA = headB;
+        }
+        
+        if (curB.next) {
+            curB = curB.next 
+        } else {
+            curB = headA;
+        }
+    }
+    
+    return curA;
 };
