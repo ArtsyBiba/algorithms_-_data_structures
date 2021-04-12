4b1280fe21a6baed9e2de28c35a6894f5ddf247f
Artsy Biba <biba.artsiom@gmail.com>
2021-04-12 10:34:45 -0400

    solving check if straight line problem

--- LC_easy/checkStraightLine.js
+++ LC_easy/checkStraightLine.js
@@ -2,5 +2,14 @@
 // represents the coordinate of a point. Check if these points make a straight line in the XY plane.
 
 var checkStraightLine = function(coordinates) {
+    let xDiff = coordinates[1][0] - coordinates[0][0];
+    let yDiff = coordinates[1][1] - coordinates[0][1];
     
+    for(let i = 2; i < coordinates.length; i++) {
+        let currXDiff = coordinates[i][0] - coordinates[i-1][0];
+        let currYDiff = coordinates[i][1] - coordinates[i-1][1];
+        
+        if(xDiff*currYDiff !== yDiff*currXDiff) return false;
+    }
+    return true;
 };
