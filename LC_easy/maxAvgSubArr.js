3a613e7a75064dff58fa33d845391ca5c7397093
Artsy Biba <biba.artsiom@gmail.com>
2021-04-13 18:54:38 -0400

    slightly optimized solution for max avg subarray problem

--- LC_easy/maxAvgSubArr.js
+++ LC_easy/maxAvgSubArr.js
@@ -16,3 +16,24 @@ var findMaxAverage = function(nums, k) {
     
     return max;
 };
+
+// slightly optimized
+var findMaxAverage = function(nums, k) {
+    if (nums.length === 1) return nums[0];
+    let max = null;
+    
+    for (let i = 0; i <= nums.length - k; i++) {
+        let counter = k - 1;
+        let sum = nums[i];
+        while (counter > 0) {
+            sum += nums[i + counter];
+            counter--;
+        }
+        
+        const avg = sum / k;
+        if (!max) max = avg
+        if (avg > max) max = avg;
+    }
+    
+    return max;
+};
