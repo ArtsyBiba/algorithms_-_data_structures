4f949511ca1e33c0d21ca71ae21ba30b11e2d8d8
Artsy Biba <biba.artsiom@gmail.com>
2021-04-13 18:44:19 -0400

    brute force solution for max avg subarray problem

--- LC_easy/maxAvgSubArr.js
+++ LC_easy/maxAvgSubArr.js
@@ -3,6 +3,16 @@
 // maximum average value. And you need to output the 
 // maximum average value.
 
+// brute force solution
 var findMaxAverage = function(nums, k) {
+    if (nums.length === 1) return nums[0];
+    let max = 0;
     
+    for (let i = 0; i <= nums.length - k; i++) {
+        const chunk = nums.slice(i, i + k);
+        const chunkAvg = chunk.reduce((a,c) => a + c) / k;
+        if (chunkAvg > max) max = chunkAvg;
+    }
+    
+    return max;
 };
