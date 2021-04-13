d5af5f2919e8438404f3ea44838a8337fd1c51bf
Artsy Biba <biba.artsiom@gmail.com>
2021-04-13 18:29:58 -0400

    solving valid perfect square problem

--- LC_easy/validPerfSq.js
+++ LC_easy/validPerfSq.js
@@ -3,5 +3,15 @@
 // Follow up: Do not use any built-in library function such as sqrt.
 
 var isPerfectSquare = function(num) {
+    if(num === 1) return true;
+  
+    let add = 3;
+    let curr = 1;
     
+    while(curr < num) {
+        curr += add;
+        add += 2;
+    }
+    
+    return curr === num;
 };
