e7677be5e64555072d59e450c7e18141374d4e3b
Artsy Biba <biba.artsiom@gmail.com>
2021-04-13 19:32:02 -0400

    solving set mismatch problem

--- LC_easy/setMismatch.js
+++ LC_easy/setMismatch.js
@@ -11,5 +11,21 @@
 // is missing and return them in the form of an array.
 
 var findErrorNums = function(nums) {
+    let i = 0;
     
+    while (i < nums.length) {
+        if (nums[i] !== i + 1 && nums[nums[i] - 1] !== nums[i]) {
+            const temp = nums[i];
+            nums[i] = nums[nums[i] - 1];
+            nums[temp - 1] = temp;
+        } else {
+            i++;
+        }
+    }
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) {
+            return [nums[i], i + 1];
+        }
+    }
 };
