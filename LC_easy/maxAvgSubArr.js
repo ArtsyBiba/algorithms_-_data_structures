ffaf240492b0ba5c3c3c764b49da072bf82f03f7
Artsy Biba <biba.artsiom@gmail.com>
2021-04-13 18:35:05 -0400

    setting up max avg subarray problem

---
+++ LC_easy/maxAvgSubArr.js
@@ -0,0 +1,8 @@
+// Given an array consisting of n integers, find the 
+// contiguous subarray of given length k that has the 
+// maximum average value. And you need to output the 
+// maximum average value.
+
+var findMaxAverage = function(nums, k) {
+    
+};
