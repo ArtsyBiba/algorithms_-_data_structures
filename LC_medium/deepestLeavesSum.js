754e845b88f7d9cc4f549f2e8f57cb9604b5b916
Artsy Biba <biba.artsiom@gmail.com>
2021-04-14 18:57:13 -0400

    solving deepest leaves sum problem

--- LC_medium/deepestLeavesSum.js
+++ LC_medium/deepestLeavesSum.js
@@ -1,5 +1,23 @@
 // Given the root of a binary tree, return the sum of values of its deepest leaves.
 
 var deepestLeavesSum = function(root) {
+    let sum = 0;
+    let deepestLevel = 0;
+
+    const traverse = (node, level) => {
+        if (!node) return;
+        
+        if (level === deepestLevel) {
+            sum += node.val;    
+        } else if (level > deepestLevel) {
+            deepestLevel = level;    
+            sum = node.val;
+        }
+        
+        traverse(node.left, level + 1);
+        traverse(node.right, level + 1);
+    }
     
+    traverse(root, 0);
+    return sum;
 };
