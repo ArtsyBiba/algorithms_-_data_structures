d5095b5b8058e4b75d08d4cad17908aa33081c6a
Artsy Biba <biba.artsiom@gmail.com>
2021-04-14 18:16:42 -0400

    setting up range sum of BST problem

---
+++ LC_easy/rangeSumBST.js
@@ -0,0 +1,6 @@
+// Given the root node of a binary search tree, return 
+// the sum of values of all nodes with a value in the range [low, high].
+
+var rangeSumBST = function(root, low, high) {
+    
+};
