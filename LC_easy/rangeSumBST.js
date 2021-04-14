8338ce8fe73f935d3068197fb0a8037e9feaff50
Artsy Biba <biba.artsiom@gmail.com>
2021-04-14 18:35:41 -0400

    solving range sum of BST problem

--- LC_easy/rangeSumBST.js
+++ LC_easy/rangeSumBST.js
@@ -2,5 +2,15 @@
 // the sum of values of all nodes with a value in the range [low, high].
 
 var rangeSumBST = function(root, low, high) {
+    let sum = 0;
+    const arr = [root];
     
+    while(arr.length) {
+        const node = arr.shift();
+        if(node.val >= low && node.val <= high) sum += node.val;
+        if(node.left) arr.push(node.left);
+        if(node.right) arr.push(node.right);
+    }
+    
+    return sum;
 };
