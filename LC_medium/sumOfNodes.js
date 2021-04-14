85482a6c7a6993efc3f6963dca9bc7259a25b3ec
Artsy Biba <biba.artsiom@gmail.com>
2021-04-14 19:08:48 -0400

    solving sum of nodes with even-valued grandparent problem

--- LC_medium/sumOfNodes.js
+++ LC_medium/sumOfNodes.js
@@ -5,5 +5,20 @@
 // If there are no nodes with an even-valued grandparent, return 0.
 
 var sumEvenGrandparent = function(root) {
+    if (!root) return 0;
+    let sum = 0;
+    const left = root.left;
+    const right = root.right;
     
+    if (root.val % 2 === 0) {
+        sum += left?.left?.val || 0;
+        sum += left?.right?.val || 0;
+        sum += right?.left?.val || 0;
+        sum += right?.right?.val || 0;
+    };
+        
+    sum += left && sumEvenGrandparent(left);
+    sum += right && sumEvenGrandparent(right);
+        
+    return sum;
 };
