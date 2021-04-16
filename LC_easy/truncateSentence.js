b048761919396d7f5c4343121a6cec119209bb46
Artsy Biba <biba.artsiom@gmail.com>
2021-04-16 18:32:11 -0400

    solving truncate sentence problem using JS helper methods

--- LC_easy/truncateSentence.js
+++ LC_easy/truncateSentence.js
@@ -8,6 +8,7 @@
 // You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate 
 // s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
 
+// using JS helpers
 var truncateSentence = function(s, k) {
-    
+    return s.split(' ').slice(0, k).join(' ');
 };
