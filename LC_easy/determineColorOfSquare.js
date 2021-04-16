83502f47ba1334aa6450491ffb53cb6d0e74b7fd
Artsy Biba <biba.artsiom@gmail.com>
2021-04-16 18:44:54 -0400

    determine color of a chessboard square problem

---
+++ LC_easy/determineColorOfSquare.js
@@ -0,0 +1,6 @@
+// You are given coordinates, a string that represents the coordinates 
+// of a square of the chessboard. Below is a chessboard for your reference.
+
+var squareIsWhite = function(coordinates) {
+    
+};
