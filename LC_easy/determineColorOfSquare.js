733c80de1945406cb6d7825a22265f32a887f84e
Artsy Biba <biba.artsiom@gmail.com>
2021-04-16 19:41:55 -0400

    solving color of a chessboard square problem

--- LC_easy/determineColorOfSquare.js
+++ LC_easy/determineColorOfSquare.js
@@ -2,5 +2,18 @@
 // of a square of the chessboard. Below is a chessboard for your reference.
 
 var squareIsWhite = function(coordinates) {
+    const letters = 'abcdefgh';
+    const map = {};
     
+    for (let i = 0; i < letters.length; i++) {
+        map[letters[i]] = i + 1;
+    }
+    
+    const letter = coordinates[0];
+    const number = Number(coordinates[1]);
+        
+    if (number % 2 !== 0 && map[letter] % 2 !== 0) return false;
+    if (number % 2 === 0 && map[letter] % 2 === 0) return false;
+    
+    return true;
 };
