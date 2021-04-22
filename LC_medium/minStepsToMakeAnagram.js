69b58c134207e39a68c97b572d0f48c24fde6456
Artsy Biba <biba.artsiom@gmail.com>
2021-04-22 08:22:23 -0400

    solving min number of steps to make two strings anagram problem

--- LC_medium/minStepsToMakeAnagram.js
+++ LC_medium/minStepsToMakeAnagram.js
@@ -7,5 +7,30 @@
 // characters with a different (or the same) ordering.
 
 var minSteps = function(s, t) {
+    const mapS = createMap(s);
+    const mapT = createMap(t);
+    let result = 0;
+    console.log(mapS)
+    console.log(mapT)
+    for (let key in mapS) {
+        if (!mapT[key]) {
+            result += mapS[key];
+        } else if (mapT[key] < mapS[key]) {
+            result += mapS[key] - mapT[key];
+        }
+    }
     
+    return result;
 };
+
+const createMap = (str) => {
+    const map = {};
+
+    for (let char of str) {
+        if (map[char]) {
+            map[char]++;
+        } else map[char] = 1;
+    }
+
+    return map;
+}
