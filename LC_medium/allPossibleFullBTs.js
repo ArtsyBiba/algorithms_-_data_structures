70da7aae0ac90aaf72d222c4b85d75b71685e60a
Artsy Biba <biba.artsiom@gmail.com>
2021-04-23 08:19:05 -0400

    solving all possible full BTs problem

--- LC_medium/allPossibleFullBTs.js
+++ LC_medium/allPossibleFullBTs.js
@@ -7,5 +7,24 @@
 // A full binary tree is a binary tree where each node has exactly 0 or 2 children.
 
 var allPossibleFBT = function(n) {
+    if (n % 2 === 0) return [];
+    if (n === 1) return [new TreeNode(0)];
     
+    let trees = [];
+    
+    for (let i = 1; i <= n - 2; i += 2) {
+        let leftTrees = allPossibleFBT(i);
+        let rightTrees = allPossibleFBT(n - 1 - i);
+        
+        leftTrees.forEach(lt => {
+            rightTrees.forEach(rt => {
+                let root = new TreeNode(0);
+                root.left = lt;
+                root.right = rt;
+                trees.push(root);
+            });
+        });
+    }
+    
+    return trees;
 };
