2ff0962ab87865a04eda77d38e859ef5db2efb96
Artsy Biba <biba.artsiom@gmail.com>
2021-04-23 08:03:03 -0400

    setting up all possible full BTs problem

--- LC_medium/countNumOfTeams.js
+++ LC_medium/countNumOfTeams.js
@@ -6,6 +6,7 @@
 // A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
 // Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
 
+// initial solution
 var numTeams = function(rating) {
     let count = 0;
     
@@ -20,6 +21,23 @@ var numTeams = function(rating) {
             } else {
                 count += rating.slice(j + 1).filter(item => item > second).length;
             }  
+        }
+    }
+    
+    return count;
+};
+
+// optimized solution
+var numTeams = function(rating) {
+    let count = 0;
+    
+    for (let i = 0; i < rating.length - 2; i++) {
+        for (let j = i + 1; j < rating.length - 1; j++) {
+            for (let k = j + 1; k < rating.length; k++) {
+                if ((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])) {
+                    count++;
+                }
+            }
         }
     }
     
