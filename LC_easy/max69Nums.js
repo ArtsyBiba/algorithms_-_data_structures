d65283ff453e85b8ed0bd436cd95632da4dc2d15
Artsy Biba <biba.artsiom@gmail.com>
2021-04-26 08:00:04 -0400

    solving max 69 num problem

--- LC_easy/max69Nums.js
+++ LC_easy/max69Nums.js
@@ -3,5 +3,12 @@
 // Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
 
 var maximum69Number  = function(num) {
+    for (let i = 0; i < num.toString().length; i++) {
+        let cur = num.toString();
+        if (cur[i] === '6') {
+            return cur.slice(0, i) + '9' + cur.slice(i + 1);
+        }
+    }
     
+    return num;
 };
