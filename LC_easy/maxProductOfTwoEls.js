b6c33ba5980c5ae3cbf6edfa5fc256153135cd45
Artsy Biba <biba.artsiom@gmail.com>
2021-04-26 08:19:29 -0400

    solving max product of two elements problem

--- LC_easy/maxProductOfTwoEls.js
+++ LC_easy/maxProductOfTwoEls.js
@@ -2,6 +2,16 @@
 // different indices i and j of that array. Return the maximum 
 // value of (nums[i]-1)*(nums[j]-1).
 
+// nested for loop solution
 var maxProduct = function(nums) {
+    let max = 0;
     
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let cur = (nums[i] - 1) * (nums[j] - 1);
+            if (cur > max) max = cur;
+        }
+    }
+    
+    return max;
 };
