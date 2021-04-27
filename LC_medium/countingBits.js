a1ec7782578f7e8e187a37225dffde66e2455dbd
Artsy Biba <biba.artsiom@gmail.com>
2021-04-27 06:49:53 -0400

    solving counting bits problem

--- LC_medium/countingBits.js
+++ LC_medium/countingBits.js
@@ -2,5 +2,14 @@
 // in the binary representation of every number in the range [0, num].
 
 var countBits = function(num) {
+    const result = [0];
+    let count = 0;
     
+    while (count < num) {
+        count++;
+        let binary = count.toString(2).split('').reduce((a,c) => Number(a) + Number(c));
+        result.push(binary);
+    }
+    
+    return result;
 };
