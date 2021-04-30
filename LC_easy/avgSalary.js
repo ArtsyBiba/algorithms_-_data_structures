05eca75b18017374d05f11d94631bda4f42fac8e
Artsy Biba <biba.artsiom@gmail.com>
2021-04-30 08:39:23 -0400

    solving average salary problem

--- LC_easy/avgSalary.js
+++ LC_easy/avgSalary.js
@@ -3,5 +3,15 @@
 // Return the average salary of employees excluding the minimum and maximum salary.
 
 var average = function(salary) {
+    let min = salary[0];
+    let max = salary[0];
+    let sum = 0;
     
+    for (const num of salary) {
+        sum += num;
+        if (num < min) min = num;
+        if (num > max) max = num;
+    }
+    
+    return (sum - (min + max)) / (salary.length - 2)
 };
