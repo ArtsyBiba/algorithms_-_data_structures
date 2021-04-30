ca701e047488fe4d66f897051f641973fb9bfdec
Artsy Biba <biba.artsiom@gmail.com>
2021-04-30 09:36:56 -0400

    solving pseudo palindromic path in BT problem

--- LC_medium/pseudoPalindromeBT.js
+++ LC_medium/pseudoPalindromeBT.js
@@ -4,6 +4,49 @@
 
 // Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
 
-var pseudoPalindromicPaths  = function(root) {
-    
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var pseudoPalindromicPaths  = function(root) {
+    let count = 0;
+    const stack = [{ node: root, pathMap: new Array(10).fill(0) }];
+
+    while (stack.length > 0) {
+        const { node, pathMap } = stack.pop();
+        if (node) {
+            const newPathMap = [...pathMap];
+            newPathMap[node.val]++;
+
+            if (!node.left && !node.right) {
+                if (checkPalindromic(newPathMap)) count++;
+            } else {
+                    stack.push({ node: node.right, pathMap: newPathMap });
+                    stack.push({ node: node.left, pathMap: newPathMap });
+            }
+        }
+    }
+
+    return count;
+};
+
+const checkPalindromic = (pathMap) => {
+    let oddNum = 0;
+
+    for (let i = 1; i <= 9; i++) {
+        if (pathMap[i] % 2 === 1) {
+            oddNum++;
+            if (oddNum > 1) return false;
+        }
+    }
+
+    return true;
 };
