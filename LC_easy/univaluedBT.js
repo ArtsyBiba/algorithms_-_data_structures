704976948c9d618dc3fd3f8da1970f9f69639577
Artsy Biba <biba.artsiom@gmail.com>
2021-04-30 09:13:03 -0400

    iterative solution for univalued BT problem

--- LC_easy/univaluedBT.js
+++ LC_easy/univaluedBT.js
@@ -2,6 +2,17 @@
 
 // Return true if and only if the given tree is univalued.
 
+// iterative solution
 var isUnivalTree = function(root) {
+    let arr = [root];
+    const val = root.val;
     
+    while (arr.length) {
+        let node = arr.shift();
+        if (node.val !== val) return false;
+        if (node.left) arr.unshift(node.left);
+        if (node.right) arr.unshift(node.right);
+    }
+    
+    return true;
 };
