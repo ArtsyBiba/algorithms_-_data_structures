1a17f746aea80c29ff9518f42d956453a945aec0
Artsy Biba <biba.artsiom@gmail.com>
2021-04-30 08:33:00 -0400

    setting up average salary problem

---
+++ LC_easy/avgSalary.js
@@ -0,0 +1,7 @@
+// Given an array of unique integers salary where salary[i] is the salary of the employee i.
+
+// Return the average salary of employees excluding the minimum and maximum salary.
+
+var average = function(salary) {
+    
+};
