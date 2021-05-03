b2fe4b89e351b92deb4d18187dd5f5f079e5ce29
Artsy Biba <biba.artsiom@gmail.com>
2021-05-03 08:21:53 -0400

    setting up find words by chars problem

---
+++ LC_easy/findWordsByChars.js
@@ -0,0 +1,9 @@
+// You are given an array of strings words and a string chars.
+
+// A string is good if it can be formed by characters from chars (each character can only be used once).
+
+// Return the sum of lengths of all good strings in words.
+
+var countCharacters = function(words, chars) {
+    
+};
