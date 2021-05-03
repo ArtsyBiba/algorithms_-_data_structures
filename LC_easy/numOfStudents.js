d8bce975ec976f113d5da2e2b42017a6111e1a12
Artsy Biba <biba.artsiom@gmail.com>
2021-05-03 09:32:41 -0400

    solving num of students problem

--- LC_easy/numOfStudents.js
+++ LC_easy/numOfStudents.js
@@ -9,5 +9,19 @@
 // You are given two integer arrays students and sandwiches where sandwiches[i] is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack) and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is the front of the queue). Return the number of students that are unable to eat.
 
 var countStudents = function(students, sandwiches) {
+    let steps = 0;
+    const maxSteps = students.length ** 2;
     
+    while (steps <= maxSteps) {
+        if (students[0] !== sandwiches[0]) {
+            students.push(students.shift());
+            steps++;
+        } else {
+            students.shift();
+            sandwiches.shift();
+            steps++;
+        }
+    }
+    
+    return students.length;    
 };
