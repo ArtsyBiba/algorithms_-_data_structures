74329b299e8c7554395191fd250c2161630f910a
Artsy Biba <biba.artsiom@gmail.com>
2021-05-03 08:46:18 -0400

    solving find words by chars problem

--- LC_easy/findWordsByChars.js
+++ LC_easy/findWordsByChars.js
@@ -4,6 +4,35 @@
 
 // Return the sum of lengths of all good strings in words.
 
-var countCharacters = function(words, chars) {
+ var countCharacters = function(words, chars) {
+    let count = 0;
+    const map = createMap(chars);
     
+    for (const word of words) {
+        let pass = true;
+        const wordMap = createMap(word);
+        
+        for (const char in wordMap) {
+            if (!map[char] || wordMap[char] > map[char]) {
+                pass = false;
+                continue;
+            }
+        }
+        
+        if (pass) count += word.length;
+    }
+    
+    return count;
 };
+
+const createMap = function(chars) {
+    const map = {};
+    
+    for (const char of chars) {
+        if (map[char]) {
+            map[char]++;
+        } else map[char] = 1;
+    }
+    
+    return map;
+}
