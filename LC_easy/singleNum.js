06bde30c4596245c4425776e6bf0a9211ee84b3a
Artsy Biba <biba.artsiom@gmail.com>
2021-05-04 08:48:35 -0400

    initial solution for single number problem

--- LC_easy/singleNum.js
+++ LC_easy/singleNum.js
@@ -4,6 +4,17 @@
 // Follow up: Could you implement a solution with a 
 // linear runtime complexity and without using extra memory?
 
+// initial solution
 var singleNumber = function(nums) {
+    const map = {};
     
+    for (const num of nums) {
+        if (map[num]) {
+            map[num]++;
+        } else map[num] = 1;
+    }
+    
+    for (const key in map) {
+        if (map[key] === 1) return key;
+    }
 };
