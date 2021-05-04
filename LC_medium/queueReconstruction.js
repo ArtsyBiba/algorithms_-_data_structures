11b3bc257a4c746393c4c6e623fd0e33a9afb408
Artsy Biba <biba.artsiom@gmail.com>
2021-05-04 09:36:20 -0400

    solving queue reconstruction exercise

--- LC_medium/queueReconstruction.js
+++ LC_medium/queueReconstruction.js
@@ -9,5 +9,21 @@
 // (queue[0] is the person at the front of the queue).
 
 var reconstructQueue = function(people) {
-    
-};
+    people.sort(sortPeople);
+    const result = [];
+     
+    for(let i = 0; i < people.length; i++) {
+        result.splice(people[i][1], 0, people[i]);
+    }
+     
+    return result;
+ };
+ 
+ var sortPeople = function(a, b) {
+      if(a[0] !== b[0]) {
+         return b[0] - a[0];
+      } else {
+         return a[1] - b[1];
+      }
+  }
+  
