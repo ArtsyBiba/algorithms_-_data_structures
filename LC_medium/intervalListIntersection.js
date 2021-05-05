bc53cb973abebdf718f64586f23c2b39f0dbb780
Artsy Biba <biba.artsiom@gmail.com>
2021-05-05 08:41:23 -0400

    solving interval list intersections problem

--- LC_medium/intervalListIntersection.js
+++ LC_medium/intervalListIntersection.js
@@ -11,5 +11,20 @@
 // intersection of [1, 3] and [2, 4] is [2, 3].
 
 var intervalIntersection = function(firstList, secondList) {
+    let indexOne = 0;
+    let indexTwo = 0;
+    const result = [];
     
+    while (indexOne < firstList.length && indexTwo < secondList.length) {
+        let maxStart = Math.max(firstList[indexOne][0], secondList[indexTwo][0]);
+        let minEnd = Math.min(firstList[indexOne][1], secondList[indexTwo][1]);
+        
+        if (maxStart <= minEnd) result.push([maxStart, minEnd]);
+        
+        if (firstList[indexOne][1] < secondList[indexTwo][1]) {
+            indexOne++;
+        } else indexTwo++;
+    }
+    
+    return result; 
 };
