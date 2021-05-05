c06f563c409389146d70e3a5042bb3beb3c282ce
Artsy Biba <biba.artsiom@gmail.com>
2021-05-05 09:23:22 -0400

    solving average of levels in BT problem

--- LC_easy/avgOfLevelsBT.js
+++ LC_easy/avgOfLevelsBT.js
@@ -3,5 +3,29 @@
 // within 10-5 of the actual answer will be accepted.
 
 var averageOfLevels = function(root) {
+    const arr = [root, 's'];
+    const res = [];
+    let sum = 0;
+    let count = 0;
     
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if (node === 's') {
+            res.push(sum / count);
+            sum = 0;
+            count = 0;
+            arr.push('s')
+        } else {
+            sum += node.val;
+            count++;
+            if (node.left) arr.push(node.left);
+            if (node.right) arr.push(node.right);
+        }
+    }
+    
+    res.push(sum / count);
+    sum = 0;
+    count = 0;
+        
+    return res;
 };
