22cb8c28a8317f7c86378b79bb9f27062443e2a1
Artsy Biba <biba.artsiom@gmail.com>
2021-05-06 08:39:16 -0400

    solving toeplitz matrix problem

--- LC_easy/toeptlizMatrix.js
+++ LC_easy/toeptlizMatrix.js
@@ -3,5 +3,12 @@
 // A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
 
 var isToeplitzMatrix = function(matrix) {
+    for (let i = 0; i < matrix.length - 1; i++) {
+        for (let j = 0; j < matrix[0].length - 1; j++) {
+            if (matrix[i + 1][j + 1] === matrix[i][j]) continue;
+            return false;
+        }
+    }
     
+    return true;
 };
