118175a193bc1880ba5cfeea948e79657642d341
Artsy Biba <biba.artsiom@gmail.com>
2021-05-06 09:20:58 -0400

    optimized solution for num of good ways to split a str problem

--- LC_medium/waysToSplitStr.js
+++ LC_medium/waysToSplitStr.js
@@ -5,13 +5,44 @@
 
 // Return the number of good splits you can make in s.
 
-// brute force solution
+// brute force solutiong
 var numSplits = function(s) {
     let count = 0;
     
     for (let i = 1; i < s.length; i++) {
         const left = createMap(s.slice(0, i));
         const right = createMap(s.slice(i));
+
+        if (Object.keys(left).length === Object.keys(right).length) count++;
+    }
+    
+    return count;
+};
+
+const createMap = function(sub) {
+    const map = {};
+    
+    for (const char of sub) {
+        if (map[char]) {
+            map[char]++;
+        } else map[char] = 1;
+    }
+    
+    return map;
+}
+
+// optimized solution
+var numSplits = function(s) {
+    let count = 0;
+    const left = {};
+    const right = createMap(s);
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        left[char] = 1;
+        if (right[char] === 1) {
+            delete right[char];
+        } else right[char]--;
 
         if (Object.keys(left).length === Object.keys(right).length) count++;
     }
