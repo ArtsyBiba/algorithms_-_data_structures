00e1f92c14e607a922ee9606ffcb5075f1d61d14
Artsy Biba <biba.artsiom@gmail.com>
2021-05-09 08:10:43 -0400

    setting up find complement problem

---
+++ LC_easy/findComplement.js
@@ -0,0 +1,6 @@
+// Given a positive integer num, output its complement number. 
+// The complement strategy is to flip the bits of its binary representation.
+
+var findComplement = function(num) {
+    
+};
