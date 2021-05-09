e219285a485e2444abed22f14e978265541ba2b6
Artsy Biba <biba.artsiom@gmail.com>
2021-05-09 08:28:28 -0400

    solving find complement problem

--- LC_easy/findComplement.js
+++ LC_easy/findComplement.js
@@ -2,5 +2,15 @@
 // The complement strategy is to flip the bits of its binary representation.
 
 var findComplement = function(num) {
+    const bin = num.toString(2);
     
+    let reverse = '';
+    
+    for (const char of bin) {
+        if (char === '1') {
+            reverse += '0';
+        } else reverse += '1';
+    }
+    
+    return parseInt(reverse, 2);  
 };
