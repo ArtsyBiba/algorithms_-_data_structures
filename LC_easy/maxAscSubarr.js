865264f10965c23629c08eb7ec84832fd69c1cd9
Artsy Biba <biba.artsiom@gmail.com>
2021-05-09 08:51:10 -0400

    solving max ascending subarr problem

--- LC_easy/maxAscSubarr.js
+++ LC_easy/maxAscSubarr.js
@@ -7,5 +7,15 @@
 // for all i where l <= i < r, numsi < numsi+1. Note that a subarray of size 1 is ascending.
 
 var maxAscendingSum = function(nums) {
+    let currSum = nums[0];
+    let maxSum = nums[0];
     
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            currSum += nums[i];
+            if (currSum > maxSum) maxSum = currSum;
+        } else currSum = nums[i];
+    }
+    
+    return maxSum;
 };
