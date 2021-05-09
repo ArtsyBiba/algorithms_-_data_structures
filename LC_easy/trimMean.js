a89541a5a0b04516316297be26716d6967a456fc
Artsy Biba <biba.artsiom@gmail.com>
2021-05-09 09:03:15 -0400

    solving mean of arr problem

--- LC_easy/trimMean.js
+++ LC_easy/trimMean.js
@@ -4,5 +4,17 @@
 // Answers within 10-5 of the actual answer will be considered accepted
 
 var trimMean = function(arr) {
+    arr.sort((a,b) => a - b);
     
+    let toRemove = Math.floor(arr.length * 0.05);
+    while (toRemove) {
+        arr.pop();
+        arr.shift();
+        toRemove--;
+    }
+    
+    const remLength = arr.length;
+    const remSum = arr.reduce((a,c) => a + c);
+    
+    return remSum / remLength;
 };
