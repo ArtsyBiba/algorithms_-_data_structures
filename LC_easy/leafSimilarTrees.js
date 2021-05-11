e2e4e55edd09a5698aec30b078628f30f7071946
Artsy Biba <biba.artsiom@gmail.com>
2021-05-11 07:59:25 -0400

    solving leaf-similar trees problem

--- LC_easy/leafSimilarTrees.js
+++ LC_easy/leafSimilarTrees.js
@@ -2,5 +2,31 @@
 // the values of those leaves form a leaf value sequence.
 
 var leafSimilar = function(root1, root2) {
+    const arr1 = [];
+    const arr2 = [];
     
+    checkLeafs(root1, arr1);
+    checkLeafs(root2, arr2);
+    
+    return isSimilar(arr1, arr2)
 };
+
+const checkLeafs = function(root, arr) {
+    if (!root.left && !root.right) {
+        arr.push(root.val);
+        return arr;
+    }
+
+    if (root.left) checkLeafs(root.left, arr);
+    if (root.right) checkLeafs(root.right, arr);
+};
+
+const isSimilar = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    
+    return true;
+}
