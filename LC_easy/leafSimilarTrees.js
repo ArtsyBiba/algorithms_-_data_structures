09d1719803aef0ff5eb2e465bfb4629d223690c8
Artsy Biba <biba.artsiom@gmail.com>
2021-05-11 07:43:31 -0400

    setting up leaf-similar trees problem

---
+++ LC_easy/leafSimilarTrees.js
@@ -0,0 +1,6 @@
+// Consider all the leaves of a binary tree, from left to right order, 
+// the values of those leaves form a leaf value sequence.
+
+var leafSimilar = function(root1, root2) {
+    
+};
