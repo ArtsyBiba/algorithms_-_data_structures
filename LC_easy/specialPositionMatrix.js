ad8c6bafc2a6e456912907a2ca8a0e1549ed51b7
Artsy Biba <biba.artsiom@gmail.com>
2021-05-12 09:06:10 -0400

    solving special positions in a binary matrix problem

--- LC_easy/specialPositionMatrix.js
+++ LC_easy/specialPositionMatrix.js
@@ -5,5 +5,26 @@
 // all other elements in row i and column j are 0 (rows and columns are 0-indexed).
 
 var numSpecial = function(mat) {
+    let rowsSum = new Array(mat.length).fill(0);
+    let columnsSum = new Array(mat[0].length).fill(0);
     
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[0].length; j++) {
+            if (mat[i][j] === 1) {
+                rowsSum[i]++;
+                columnsSum[j]++;
+            }
+        }
+    }
+
+    let result = 0;
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[0].length; j++) {
+            if (mat[i][j] === 1 && columnsSum[j] === 1 && rowsSum[i] === 1) {
+                result++;
+            }
+        }
+    }
+    
+    return result;
 };
