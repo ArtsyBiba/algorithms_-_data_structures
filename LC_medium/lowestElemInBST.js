c78d5b9348540f8e32a5b61f83da13d6582dc637
Artsy Biba <biba.artsiom@gmail.com>
2021-05-12 08:21:33 -0400

    solving Kth smallest element in a BST problem

--- LC_medium/lowestElemInBST.js
+++ LC_medium/lowestElemInBST.js
@@ -2,5 +2,16 @@
 // return the kth (1-indexed) smallest element in the tree.
 
 var kthSmallest = function(root, k) {
+    const nums = [];
+    const arr = [root];
     
+    while (arr.length) {
+        let node = arr.shift();
+        nums.push(node.val);
+        if (node.left) arr.push(node.left);
+        if (node.right) arr.push(node.right);
+    }
+    
+    nums.sort((a, b) => a - b);
+    return nums[k - 1]; 
 };
