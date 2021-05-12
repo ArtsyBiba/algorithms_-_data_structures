a66ea054685f659f19c99ae2bf5b1ff542f3df17
Artsy Biba <biba.artsiom@gmail.com>
2021-05-12 08:00:43 -0400

    brute force solution for sum of absolute difference problem

--- LC_medium/sumOfAbsoluteDiff.js
+++ LC_medium/sumOfAbsoluteDiff.js
@@ -7,6 +7,19 @@
 // In other words, result[i] is equal to sum(|nums[i]-nums[j]|) where 
 // 0 <= j < nums.length and j != i (0-indexed).
 
+// brute force
 var getSumAbsoluteDifferences = function(nums) {
+    const result = [];
     
+    for (let i = 0; i < nums.length; i++) {
+        let sum = 0;
+        
+        for (let j = 0; j < nums.length; j++) {
+            sum += Math.abs(nums[i] - nums[j]);
+        }
+        
+        result.push(sum);
+    }
+    
+    return result;
 };
