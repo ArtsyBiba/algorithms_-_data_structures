ba39fbdb664eedbe4668893551e9d88087ee9094
Artsy Biba <biba.artsiom@gmail.com>
2021-05-13 09:30:19 -0400

    solving last stone weight problem

--- LC_easy/lastStoneWeight.js
+++ LC_easy/lastStoneWeight.js
@@ -8,5 +8,20 @@
 // At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
 
 var lastStoneWeight = function(stones) {
+    stones.sort((a,b) => b - a);
     
+    while (stones.length > 1) {
+        const one = stones.shift();
+        const two = stones.shift();
+        
+        if (one === two) {
+            continue;
+        } else {
+            const toAdd = Math.abs(one - two);
+            stones.push(toAdd);
+            stones.sort((a,b) => b - a);
+        }
+    }
+    
+    return stones.length ? stones[0] : 0;
 };
