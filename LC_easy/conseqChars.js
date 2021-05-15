5c1d18523217c0f948a62f8a08cd93b3c03f6a34
Artsy Biba <biba.artsiom@gmail.com>
2021-05-15 13:34:05 -0400

    solving the consecutive chars problem

--- LC_easy/conseqChars.js
+++ LC_easy/conseqChars.js
@@ -4,5 +4,15 @@
 // Return the power of the string.
 
 var maxPower = function(s) {
+    let curr = 1;
+    let max = 1;
     
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === s[i + 1]) {
+            curr++;
+            if (curr > max) max = curr;
+        } else curr = 1;
+    }
+    
+    return max;
 };
