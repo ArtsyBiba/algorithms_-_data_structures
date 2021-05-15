c508c4e56ce18a38945ae866a130171df3b0f9bf
Artsy Biba <biba.artsiom@gmail.com>
2021-05-15 13:02:04 -0400

    solving max num of balloons problem

--- LC_easy/maxNumOfBalloons.js
+++ LC_easy/maxNumOfBalloons.js
@@ -5,5 +5,35 @@
 // the maximum number of instances that can be formed.
 
 var maxNumberOfBalloons = function(text) {
+    const mapOne = {};
+    const mapTwo = {};
+    const str = 'balloon';
     
+    for (const char of str) {
+        if (mapOne[char]) {
+            mapOne[char]++;
+        } else mapOne[char] = 1;
+    }
+    
+    for (const char of text) {
+        if (mapTwo[char]) {
+            mapTwo[char]++;
+        } else mapTwo[char] = 1;
+    }
+    
+    let result = 0;
+    
+    while(mapTwo['b'] >= 1) {
+        let count = 1;
+        for (const key in mapOne) {
+            if (!mapTwo[key] || mapTwo[key] < mapOne[key]) {
+                count--;
+                break;
+            }
+            mapTwo[key] = mapTwo[key] - mapOne[key];
+        }
+        result += count;
+    }
+    
+    return result;
 };
