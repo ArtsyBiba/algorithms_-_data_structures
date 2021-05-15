afdd53884559e3625b88c70caa0a35b5f9aa769e
Artsy Biba <biba.artsiom@gmail.com>
2021-05-15 13:09:53 -0400

    alternative solution for max num of balloons problem

--- LC_easy/maxNumOfBalloons.js
+++ LC_easy/maxNumOfBalloons.js
@@ -37,3 +37,35 @@ var maxNumberOfBalloons = function(text) {
     
     return result;
 };
+
+// alternative solution
+var maxNumberOfBalloons = function(text) {
+    const mapOne = {};
+    const mapTwo = {};
+    const str = 'balloon';
+    
+    for (const char of str) {
+        if (mapOne[char]) {
+            mapOne[char]++;
+        } else mapOne[char] = 1;
+    }
+    
+    for (const char of text) {
+        if (mapTwo[char]) {
+            mapTwo[char]++;
+        } else mapTwo[char] = 1;
+    }
+    
+    let result = [];
+
+    for (const key in mapOne) {
+        if (!mapTwo[key] || mapTwo[key] < mapOne[key]) {
+            result = 0;
+            break;
+        }
+        const curr = Math.floor(mapTwo[key] / mapOne[key]);
+        result.push(curr);
+    }
+    
+    return !result.length ? 0 : Math.min(...result);
+};
