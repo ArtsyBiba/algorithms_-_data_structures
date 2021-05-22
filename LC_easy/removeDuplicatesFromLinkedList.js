23646a91b6481165192614e4feea94afda659594
Artsy Biba <biba.artsiom@gmail.com>
2021-05-22 11:21:18 -0400

    brute force solution for remove duplicates from linked list problem

--- LC_easy/removeDuplicatesFromLinkedList.js
+++ LC_easy/removeDuplicatesFromLinkedList.js
@@ -1,6 +1,26 @@
 // Given the head of a sorted linked list, delete all duplicates such 
 // that each element appears only once. Return the linked list sorted as well.
 
+// brute force
 var deleteDuplicates = function(head) {
+    if (!head) return null;
+    let curr = head;
+    let next = head.next;
     
+    while (next) {
+        if (curr.val === next.val) {
+            if (next.next) {
+                curr.next = next.next;
+                next = next.next;
+            } else {
+                curr.next = null;
+                next = null;
+            }
+        } else {
+            curr = next;
+            next = next.next;
+        }
+    }
+    
+    return head;
 };
