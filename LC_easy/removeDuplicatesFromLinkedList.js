f32ee9225d88c080abd8f2c209a8e6347efcc9ce
Artsy Biba <biba.artsiom@gmail.com>
2021-05-22 11:24:59 -0400

    cleaner solution for remove duplicates from linked list problem

--- LC_easy/removeDuplicatesFromLinkedList.js
+++ LC_easy/removeDuplicatesFromLinkedList.js
@@ -24,3 +24,18 @@ var deleteDuplicates = function(head) {
     
     return head;
 };
+
+// optimized solution
+var deleteDuplicates = function(head) {
+    let curr = head;
+    
+    while (curr) {
+        if (curr.next && curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+    
+    return head;
+};
